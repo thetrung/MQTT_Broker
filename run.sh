5fb42c324e6de2c0886d06d2fa385fef1c49cefb
Nguyễn Thế Trung <deulamco@gmail.com>
2024-08-23 22:19:27 +0700

    Update run.sh with data, log & psswd_file.

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo docker run -it -p 1883:1883 -p 9001:9001 -v ./config/mosquitto.conf:/mosquitto/config/mosquitto.conf -v /mosquitto/data -v /mosquitto/log eclipse-mosquitto
+sudo docker run -it -p 1883:1883 -p 9001:9001 -v ./config/mosquitto.conf:/mosquitto/config/mosquitto.conf -v ./data:/mosquitto/data -v ./log:/mosquitto/log -v ./passwd_file:/mosquitto/passwd_file eclipse-mosquitto
